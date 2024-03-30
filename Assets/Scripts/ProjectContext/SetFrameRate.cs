911f39cb592a2c9059a751303f8196a6b87dcedd
nataliadolina <45314570+nataliadolina@users.noreply.github.com>
2024-03-30 23:01:01 +0300

    set fps

---
+++ Assets/Scripts/ProjectContext/SetFrameRate.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class SetFrameRate : MonoBehaviour
+{
+    private void Awake()
+    {
+        QualitySettings.vSyncCount = 0; 
+        Application.targetFrameRate = 45;
+    }
+}
